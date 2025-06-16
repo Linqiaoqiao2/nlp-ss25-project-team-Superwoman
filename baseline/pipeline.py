e41710e18f10157a61729cbcd4eef115b16e0739
Wenhui Deng <wenhui@maximilian-kukla.com>
2025-06-16 14:09:46 +0200

    Improve output

--- baseline/pipeline.py
+++ baseline/pipeline.py
@@ -24,9 +24,9 @@ class RAGPipeline:
     def run_chatbot(self, top_k: int = 3) -> str:
         previous_conversation = "Conversation history:\n"
         while (True):
-            query = input()
+            query = input("\nPlease type your question: ")
             top_chunks = self.retriever.query(query, top_k=top_k)
             logger.info(f"Top chunks choosen: {top_chunks}")
             result = self.generator.generate_answer(query, top_chunks, self.prompt_template, previous_conversation)
             previous_conversation += query + result
-            print()
+            print("End of answer")
