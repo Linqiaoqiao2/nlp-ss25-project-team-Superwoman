813e1e912bf0b5e4e82d71df5b813e942350d70a
Wenhui Deng <wenhui@maximilian-kukla.com>
2025-06-16 13:48:26 +0200

    Bugfix for chatbot

--- baseline/pipeline.py
+++ baseline/pipeline.py
@@ -22,10 +22,11 @@ class RAGPipeline:
         return self.generator.generate_answer(query, top_chunks, self.prompt_template)
     
     def run_chatbot(self, top_k: int = 3) -> str:
-        previous_conversation = None
+        previous_conversation = "Conversation history:\n"
         while (True):
             query = input()
             top_chunks = self.retriever.query(query, top_k=top_k)
             logger.info(f"Top chunks choosen: {top_chunks}")
             result = self.generator.generate_answer(query, top_chunks, self.prompt_template, previous_conversation)
-            previous_conversation += "\n" + query + "\n" + result
+            previous_conversation += query + result
+            print()
